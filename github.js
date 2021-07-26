6b8347e26c0090bcf34a203540a19777d63a5708
Cesar Villalvir <72565897+cesarovj@users.noreply.github.com>
2021-07-26 14:21:00 -0400

    first commit

--- github.js
+++ github.js
@@ -5,7 +5,7 @@ class Github {
         this.repos_count = 5;
         this.repos_sort = 'created: asc';
     
-    }
+    };
 
     async getUser(user) {
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
@@ -19,5 +19,5 @@ class Github {
             profile: profileData,
             repos: repoData
         }
-    }
+    };
 }
